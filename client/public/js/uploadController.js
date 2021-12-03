014132a13eda7d5dec564db98cf5676058465d3f
Rafly-Ananda <anandarafly15@gmail.com>
2021-12-03 10:21:32 +0700

    fixing upload logic attempt x

--- client/public/js/uploadController.js
+++ client/public/js/uploadController.js
@@ -11,28 +11,96 @@ const overlay = document.querySelector(".overlay");
 
 // ** Functions
 
-function postData(database, identifier, inputBox) {
-  const csvData = document.getElementById(`${inputBox}`).files[0];
-  Papa.parse(csvData, {
+// function postData(database, identifier, inputBox) {
+//   const csvData = document.getElementById(`${inputBox}`).files[0];
+//   Papa.parse(csvData, {
+//     header: true,
+//     skipEmptyLines: true,
+//     fastMode: true,
+//     complete: function (results) {
+//       const sendPostRequest = () => {
+//         try {
+//           overlay.classList.remove("hidden");
+//           const postRequest = axios.post(
+//             `/upload/${database}/${identifier}`,
+//             results.data
+//           );
+//           postRequest.then(() => {
+//             alert(`Data ${csvData.name} sudah ditambakan kedalam database`);
+//           });
+//         } catch (error) {
+//           console.log(error);
+//         }
+//       };
+//       sendPostRequest();
+//     },
+//   });
+// }
+
+function postData(identifier, inputBox, inputBox2, inputBox3, inputBox4) {
+  const csvData__1 = document.getElementById(`${inputBox}`).files[0];
+  const csvData__2 = document.getElementById(`${inputBox2}`).files[0];
+  const csvData__3 = document.getElementById(`${inputBox3}`).files[0];
+  const csvData__4 = document.getElementById(`${inputBox4}`).files[0];
+  overlay.classList.remove("hidden");
+
+  Papa.parse(csvData__1, {
     header: true,
     skipEmptyLines: true,
     fastMode: true,
     complete: function (results) {
-      const sendPostRequest = () => {
-        try {
-          overlay.classList.remove("hidden");
-          const postRequest = axios.post(
-            `/upload/${database}/${identifier}`,
-            results.data
-          );
-          postRequest.then(() => {
-            alert(`Data ${csvData.name} sudah ditambakan kedalam database`);
-          });
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      sendPostRequest();
+      axios.post(`/upload/dataone/${identifier}`, results.data).then(() => {
+        alert(`data ${csvData__1.name} sudah ditambahkan`);
+
+        Papa.parse(csvData__2, {
+          header: true,
+          skipEmptyLines: true,
+          fastMode: true,
+          complete: function (results) {
+            overlay.classList.remove("hidden");
+            axios
+              .post(`/upload/datatwo/${identifier}`, results.data)
+              .then(() => {
+                alert(`data ${csvData__2.name} sudah ditambahkan`);
+
+                Papa.parse(csvData__3, {
+                  header: true,
+                  skipEmptyLines: true,
+                  fastMode: true,
+                  complete: function (results) {
+                    overlay.classList.remove("hidden");
+                    axios
+                      .post(`/upload/datathree/${identifier}`, results.data)
+                      .then(() => {
+                        alert(`data ${csvData__3.name} sudah ditambahkan`);
+
+                        Papa.parse(csvData__4, {
+                          header: true,
+                          skipEmptyLines: true,
+                          fastMode: true,
+                          complete: function (results) {
+                            overlay.classList.remove("hidden");
+                            axios
+                              .post(
+                                `/upload/datafour/${identifier}`,
+                                results.data
+                              )
+                              .then(() => {
+                                alert(
+                                  `data ${csvData__4.name} sudah ditambahkan`
+                                );
+                                overlay.classList.add("hidden");
+                                location.reload();
+                              });
+                          },
+                        });
+                      });
+                  },
+                });
+              });
+          },
+        });
+      });
     },
   });
 }
@@ -50,9 +118,12 @@ uploadDataBtn.addEventListener("click", () => {
   ) {
     alert("Isi Semua Field Input!");
   } else {
-    postData("dataone", dataId, "uploadFile1");
-    postData("datatwo", dataId, "uploadFile2");
-    postData("datathree", dataId, "uploadFile3");
-    postData("datafour", dataId, "uploadFile4");
+    postData(
+      dataId,
+      "uploadFile1",
+      "uploadFile2",
+      "uploadFile3",
+      "uploadFile4"
+    );
   }
 });
